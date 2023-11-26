15f634abff69167797cfc09bf5758145d8ab3497
upasanakumar <upasana.kumar@sjsu.edu>
2023-11-26 00:22:17 -0800

    Updating Register error messages

--- app/src/main/java/com/example/smart_pantry/Register.java
+++ app/src/main/java/com/example/smart_pantry/Register.java
@@ -6,25 +6,6 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.amplifyframework.auth.AuthUserAttributeKey;
-import com.amplifyframework.auth.options.AuthSignUpOptions;
-import com.amplifyframework.core.Amplify;
-
-import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -54,7 +35,7 @@ public class Register extends AppCompatActivity {
         progressBarRegister = findViewById(R.id.progressBarRegister); // Initialize ProgressBar here
 
         View textView = findViewById(R.id.loginNow);
-        textView.setOnClickListener(new View.OnClickListener(){
+        textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), Login.class);
@@ -66,44 +47,43 @@ public class Register extends AppCompatActivity {
         buttonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String fullName, email, password;
+                String fullName = editTextFullName.getText().toString();
+                String email = editTextEmail.getText().toString();
+                String password = editTextPassword.getText().toString();
 
-                fullName = String.valueOf(editTextFullName.getText());
-                email = String.valueOf(editTextEmail.getText());
-                password = String.valueOf(editTextPassword.getText());
-
-                if (TextUtils.isEmpty(fullName)){
+                if (TextUtils.isEmpty(fullName)) {
                     Toast.makeText(Register.this, "Enter Full Name", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                if (TextUtils.isEmpty(email)){
+                if (TextUtils.isEmpty(email)) {
                     Toast.makeText(Register.this, "Enter Email", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                if (TextUtils.isEmpty(password)){
+                if (TextUtils.isEmpty(password)) {
                     Toast.makeText(Register.this, "Enter Password", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
-                progressBarRegister.setVisibility(View.VISIBLE); // Show the ProgressBar when registration starts
+                progressBarRegister.setVisibility(View.VISIBLE); // Show the ProgressBar
 
                 AuthSignUpOptions options = AuthSignUpOptions.builder()
                         .userAttribute(AuthUserAttributeKey.givenName(), fullName)
                         .build();
+
                 Amplify.Auth.signUp(email, password, options,
-                        result -> {
-                            progressBarRegister.setVisibility(View.GONE); // Hide the ProgressBar on successful registration
+                        result -> runOnUiThread(() -> {
+                            progressBarRegister.setVisibility(View.GONE); // Hide the ProgressBar
                             Log.i("AuthQuickStart", "Result: " + result.toString());
-                            Intent intent =  new Intent(getApplicationContext(), ConfirmSignUp.class);
+                            Intent intent = new Intent(getApplicationContext(), ConfirmSignUp.class);
                             intent.putExtra("email", email);
                             intent.putExtra("name", fullName);
                             startActivity(intent);
-                        },
-                        error -> {
-                            progressBarRegister.setVisibility(View.GONE); // Hide the ProgressBar on failure
+                        }),
+                        error -> runOnUiThread(() -> {
+                            progressBarRegister.setVisibility(View.GONE); // Hide the ProgressBar
                             Log.e("AuthQuickStart", "Sign up failed", error);
-                            Toast.makeText(Register.this, "Registration failed", Toast.LENGTH_SHORT).show();
-                        }
+                            Toast.makeText(Register.this, "Registration failed: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+                        })
                 );
             }
         });
