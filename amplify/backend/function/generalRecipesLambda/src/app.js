c10a669fb0a4dc1c6516307f65ded4d70c957f14
Aishwarya Manoharan <143560747+aishwarya-mano@users.noreply.github.com>
2023-11-30 02:40:25 -0800

    updating the general recipe Lambda function

--- amplify/backend/function/generalRecipesLambda/src/app.js
+++ amplify/backend/function/generalRecipesLambda/src/app.js
@@ -1,15 +1,3 @@
-General recipe API
-
-/*
-Copyright 2017 - 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
-    http://aws.amazon.com/apache2.0/
-or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and limitations under the License.
-*/
-
-
-
 const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
 const { DeleteCommand, DynamoDBDocumentClient, GetCommand, PutCommand, QueryCommand, ScanCommand } = require('@aws-sdk/lib-dynamodb');
 const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
